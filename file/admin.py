74e3d2ba91fa7ad0415d857388c5150bd22cf5db
alireza amini <ali.4mini@proton.me>
2023-02-20 20:08:24 +0330

    added models to admin panel and changed the language in setting.py

--- file/admin.py
+++ file/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Sell, Rent
 
 # Register your models here.
+
+@admin.register(Sell)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['owner_name', 'm2', 'address', 'type', ]
+    list_filter = ['m2', 'price', ]
+    #search_fields = ['title', 'body']
+    #prepopulated_fields = {'slug': ('title',)}
+    #raw_id_fields = ['author']
+    #date_hierarchy = 'publish'
+    #ordering = ['status', 'publish']
+
+
+@admin.register(Rent)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['owner_name', 'm2', 'address', 'type', ]
+    list_filter = ['m2', 'price_up', 'price_rent',]
