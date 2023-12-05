84c157605b32924ef9af37ed6ef3b797277b08ea
alireza amini <ali.4mini@proton.me>
2023-12-05 16:13:52 +0330

    - less password validation and made the react app ready for serve in local network

--- amlak/settings.py
+++ amlak/settings.py
@@ -148,14 +148,14 @@ ALLOWED_HOSTS = ['0.0.0.0', '87.107.54.39', '*']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
