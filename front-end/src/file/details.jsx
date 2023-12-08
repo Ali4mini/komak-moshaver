91f46353298b48cfa8d05fc3358495ff36ad0672
alireza amini <ali.4mini@proton.me>
2023-12-08 14:31:03 +0330

    - updated status

--- front-end/src/file/details.jsx
+++ front-end/src/file/details.jsx
@@ -32,31 +32,33 @@ const FileDetails = () => {
 
   const optionItems = [
     {
-      key: "print",
-      label: "چاپ فایل",
+      key: "updated",
+      label: "موجود است",
+      handler: () => {
+        var now = new Date();
+        api
+          .patch(`file/${fileType}/${id}/`, { updated_at: now })
+          .then(navigate("/", { replace: true }))
+          .catch((error) => console.log(error.data));
+      },
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
           viewBox="0 0 24 24"
           strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-5 h-6"
+          stroke="white"
+          className="w-6 h-6"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
-          />
+          <path d="M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10C22,6.48,17.52,2,12,2z M12.06,19v-2.01c-0.02,0-0.04,0-0.06,0 c-1.28,0-2.56-0.49-3.54-1.46c-1.71-1.71-1.92-4.35-0.64-6.29l1.1,1.1c-0.71,1.33-0.53,3.01,0.59,4.13c0.7,0.7,1.62,1.03,2.54,1.01 v-2.14l2.83,2.83L12.06,19z M16.17,14.76l-1.1-1.1c0.71-1.33,0.53-3.01-0.59-4.13C13.79,8.84,12.9,8.5,12,8.5c-0.02,0-0.04,0-0.06,0 v2.15L9.11,7.83L11.94,5v2.02c1.3-0.02,2.61,0.45,3.6,1.45C17.24,10.17,17.45,12.82,16.17,14.76z" />
         </svg>
       ),
     },
     {
+      key: "matched_customers",
+      label: "مشتریان مرتبط",
       handler: () => {
         setIsMatchedCustomer(!isMatchedCustomer);
       },
-      key: "matched_customers",
-      label: "مشتریان مرتبط",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -75,12 +77,12 @@ const FileDetails = () => {
       ),
     },
     {
-      handler: () => {
-        setIsDeleteConfirm(!isDeleteConfirm);
-      },
       key: "delete",
       label: "حذف",
       style: "text-red-600 ",
+      handler: () => {
+        setIsDeleteConfirm(!isDeleteConfirm);
+      },
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -168,7 +170,6 @@ const FileDetails = () => {
             <DeleteConfirm
               isOpen={isDeleteConfirm}
               setIsOpen={setIsDeleteConfirm}
-              
               title={"آیا از پاک کردن این فایل مطمعنید؟"}
               description={
                 "اگر فایل را اشتباهی پاک کردید سریعا به مدیر مجموعه اطلاع رسانی کنید"
