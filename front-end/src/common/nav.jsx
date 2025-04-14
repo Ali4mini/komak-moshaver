18937a801914850d697077a0bbd3ff003f792d83
Alireza Amini <alireza.4mini@gmail.com>
2025-04-14 08:41:42 +0000

    Revert "style: Make navbar vertical by adjusting CSS and structure"
    This reverts commit f09fa157e8a0c948b13424b03f04285f33c74b40.

--- front-end/src/common/nav.jsx
+++ front-end/src/common/nav.jsx
@@ -30,9 +30,9 @@ const NavBar = () => {
   return (
     <nav
       id="NavBar"
-      className="flex flex-col border-b mb-4 px-5 sticky top-0 h-auto border-gray-gray bg-white z-50"
+      className="flex border-b justify-between mb-4 px-5 sticky top-0 h-14 border-gray-gray bg-white  z-50"
     >
-      <div className="flex items-center gap-4 flex-col">
+      <div className="flex items-center gap-4">
 
         {
           isLogOutConfirm && <LogOutConfirm isOpen={isLogOutConfirm} setIsOpen={setIsLogOutConfirm} />
@@ -43,73 +43,75 @@ const NavBar = () => {
 
         <Link
           id="home"
-          className="inline-block border border-blue-400 rounded-lg py-1 px-3 bg-blue-400 text-white hover:bg-blue-700 active:ring-2 mb-2"
+          className="inline-block border border-blue-400 rounded-lg py-1 px-3 bg-blue-400 text-white hover:bg-blue-700 active:ring-2"
           to="dashboard/"
         >
           داشبورد
         </Link>
         <Link
           id="home"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           to="/"
         >
           فایل ها
         </Link>
-        <div className="relative inline-flex items-center flex-col">
+        <div className="relative inline-flex items-center">
           <Link
             id="listing"
-            className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+            className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
             to="listing/"
           >
             ربات
           </Link>
           <span className="absolute top-0 right-0 text-xs font-bold text-red-500 w-4 h-4">
+
             {scannerFilesCount}
           </span>
         </div>
-        <div className="relative inline-flex items-center flex-col">
+        <div className="relative inline-flex items-center">
           <Link
             id="restore"
-            className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+            className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
             to="restore/"
           >
             احیا
           </Link>
           <span className="absolute top-0 right-0 text-xs font-bold text-red-500 w-4 h-4">
+
             {restoreFilesCount}
           </span>
         </div>
         <Link
           id="new_file"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           to="file/new"
         >
           فایل جدید
         </Link>
         <Link
           id="new_customer"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           to="customer/new"
         >
           مشتری جدید
         </Link>
         <Link
           id="customers"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           to="/customers"
         >
           مشتری ها
         </Link>
         <Link
           id="smsLogs"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           to="smsLogs/"
         >
           sms
         </Link>
         <button
           id="agents"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           onClick={() => {
             setIsLogOutConfirm(true)
           }}
@@ -119,7 +121,7 @@ const NavBar = () => {
 
         <button
           id="agents"
-          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2 mb-2"
+          className="inline-block border border-gray-50 rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-1 px-3 active:ring-2"
           onClick={() => {
             setIsSearch(true)
           }}
